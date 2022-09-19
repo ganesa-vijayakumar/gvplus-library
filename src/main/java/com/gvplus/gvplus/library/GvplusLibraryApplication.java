094f6bddbe8f1b722ed6228d94c1bfcf7eed2133
Ganesa Vijayakumar <ganesa.vijayakumar@savvas.com>
2022-09-19 16:56:48 +0530

    Initial commit

---
+++ src/main/java/com/gvplus/gvplus/library/GvplusLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.gvplus.gvplus.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvplusLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GvplusLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gvplus/gvplus/library/GvplusLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvplus.gvplus.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvplusLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
